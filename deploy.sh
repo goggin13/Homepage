5e62d5c39418ed564813b0380302b9f991a818fb
Matt Goggin <goggin13@gmail.com>
2012-09-18 02:23:49 -0400

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,11 @@ ruby switch_out_javascript.rb -prod
 
 ./s3/s3cmd put --acl-public --recursive ~/projects/coffee/home/javascripts/frameworks/ s3://www.mattgoggin.com/javascripts/frameworks/
 ./s3/s3cmd put --acl-public --recursive ~/projects/coffee/home/javascripts/src/ s3://www.mattgoggin.com/javascripts/src/
+./s3/s3cmd put --acl-public ~/projects/coffee/home/javascripts/application.js s3://www.mattgoggin.com/javascripts/application.js
 ./s3/s3cmd put --acl-public --recursive ~/projects/coffee/home/stylesheets/ s3://www.mattgoggin.com/stylesheets/
 ./s3/s3cmd put --acl-public --recursive ~/projects/coffee/home/images/ s3://www.mattgoggin.com/images/
 ./s3/s3cmd put --acl-public --recursive ~/projects/coffee/home/templates/ s3://www.mattgoggin.com/templates/
-# ./s3/s3cmd put --acl-public --recursive ~/projects/coffee/home/offline.appcache s3://www.mattgoggin.com/home/offline.appcache
+# # ./s3/s3cmd put --acl-public --recursive ~/projects/coffee/home/offline.appcache s3://www.mattgoggin.com/home/offline.appcache
 ./s3/s3cmd put --acl-public --recursive ~/projects/coffee/home/test.html s3://www.mattgoggin.com/test.html
 ./s3/s3cmd put --acl-public --recursive ~/projects/coffee/home/index.html s3://www.mattgoggin.com/index.html
 ./s3/s3cmd put --acl-public --recursive ~/projects/coffee/home/index.html s3://www.mattgoggin.com/404.html
